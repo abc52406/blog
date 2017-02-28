92a519533065e83e64817332d212141b7874a80f
Bob <pb128@hotmail.com>
2017-02-28 20:01:06 +0800

    测试页面

---
+++ js/local_storage_manager.js
@@ -0,0 +1,86 @@
+window.fakeStorage = {
+  _data: {},
+
+  setItem: function (id, val) {
+    return this._data[id] = String(val);
+  },
+
+  getItem: function (id) {
+    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+  },
+
+  removeItem: function (id) {
+    return delete this._data[id];
+  },
+
+  clear: function () {
+    return this._data = {};
+  }
+};
+
+function LocalStorageManager() {
+  this.bestScoreKey     = "bestScore";
+  this.gameStateKey = "gameState";
+  this.gameLastStateKey = "gameLastState";
+  this.gameLastLength=5;
+
+  var supported = this.localStorageSupported();
+  this.storage = supported ? window.localStorage : window.fakeStorage;
+}
+
+LocalStorageManager.prototype.localStorageSupported = function () {
+  var testKey = "test";
+  var storage = window.localStorage;
+
+  try {
+    storage.setItem(testKey, "1");
+    storage.removeItem(testKey);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+// Best score getters/setters
+LocalStorageManager.prototype.getBestScore = function () {
+  return this.storage.getItem(this.bestScoreKey) || 0;
+};
+
+LocalStorageManager.prototype.setBestScore = function (score) {
+  this.storage.setItem(this.bestScoreKey, score);
+};
+
+// Game state getters/setters and clearing
+LocalStorageManager.prototype.getGameState = function () {
+  var stateJSON = this.storage.getItem(this.gameStateKey);
+  return stateJSON ? JSON.parse(stateJSON) : null;
+};
+
+LocalStorageManager.prototype.setGameState = function (gameState) {
+  this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+};
+
+LocalStorageManager.prototype.clearGameState = function () {
+  this.storage.removeItem(this.gameStateKey);
+};
+
+LocalStorageManager.prototype.getGameLastState = function () {
+	var stateJSON = this.storage.getItem(this.gameLastStateKey);
+	var list = stateJSON ? JSON.parse(stateJSON) : new Array();
+	var gameState=list.pop();
+	this.storage.setItem(this.gameLastStateKey, JSON.stringify(list));
+	return gameState;
+};
+
+LocalStorageManager.prototype.setGameLastState = function (gameState) {
+	var stateJSON = this.storage.getItem(this.gameLastStateKey);
+	var list = stateJSON ? JSON.parse(stateJSON) : new Array();
+	list.push(gameState);
+	if(list.length>this.gameLastLength)
+		list.shift();
+	this.storage.setItem(this.gameLastStateKey, JSON.stringify(list));
+};
+
+LocalStorageManager.prototype.clearGameLastState = function () {
+  this.storage.removeItem(this.gameLastStateKey);
+};
